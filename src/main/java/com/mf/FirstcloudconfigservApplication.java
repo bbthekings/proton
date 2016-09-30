76312ffecb71644abc3b4028cfe56c243053561e
bbthekings <bbthekings@gmail.com>
2016-09-30 14:52:26 +0200

    test 1

---
+++ src/main/java/com/mf/FirstcloudconfigservApplication.java
@@ -0,0 +1,12 @@
+package com.mf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstcloudconfigservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstcloudconfigservApplication.class, args);
+	}
+}
